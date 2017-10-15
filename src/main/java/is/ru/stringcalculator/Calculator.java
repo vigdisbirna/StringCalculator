dbc56469d0476b7668fdd2e9b8edc45d1b29e4ad
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2017-10-15 19:15:00 +0000

    Tjekka á negative numbers og setti inn öll testin sem ég her buin að gera, gleymdi að setja gömlu alltaf með inn í hinum commitunum

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -10,14 +10,48 @@ public class Calculator {
 		}
 
 		String[] temp = text.split("[,\\n]+");
+		String[] neg;
+		int countNeg = 0; 
 		int sum = 0;
         
+        for(int i = 0; i <temp.length; i++)
+        {
+        	if(Integer.parseInt(temp[i]) < 0)
+        		{
+        			countNeg++;
+        		}
+        } 
+
+        neg = new String[countNeg];
+        int count = 0;
+
+        for(int i = 0; i < temp.length; i++)
+        {
+        	if(Integer.parseInt(temp[i]) < 0)
+        		{
+        			neg[count] = temp[i];
+        			count++;
+        		}
+        }
+
+        if(countNeg > 0)
+        {
+        	String error = "";
+
+        	for(int i = 0; i < neg.length; i++)
+        	{
+        		error = neg[i] + ", ";
+        	}
+
+        	throw new RuntimeException("Negatives not allowed: " + error);
+        }
+
+
         for(int i = 0; i < temp.length; i++)
 		{
 			sum = Integer.parseInt(temp[i]) + sum;
 		}
 
 		return sum;
-
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,9 +8,32 @@ public class CalculatorTest {
 	
 	@Test
 	public void testEmptyString() {
-		assertEquals(7, Calculator.add("2\n1,2,2")); 
+		assertEquals(0, Calculator.add("")); 
 	}
 
+	@Test 
+	public void testSimpleAdd(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
 
+	@Test
+	public void testAdd() {
+		assertEquals(7, Calculator.add("2,1,2,2")); 
+	}
+
+	@Test
+	public void testTwoSplitters(){
+		assertEquals(8, Calculator.add("2\n4,2"));
+	}
+
+	@Test
+	public void TestNegative(){
+		try{
+
+		Calculator.add("-1, 3,-2");
+	    }catch(Exception e){
+
+	    }
+	}
 
 }
